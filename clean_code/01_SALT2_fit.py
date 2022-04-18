adbe2b10fcdd359b937de442c4c186b1f2fa8d61
emilleishida <emilleishida@gmail.com>
2022-04-18 15:51:05 +0200

    update paths to SALT2_fit

--- clean_code/01_SALT2_fit.py
+++ clean_code/01_SALT2_fit.py
@@ -3,6 +3,7 @@ from resspect.salt3_utils import get_distances
 import time
 import pandas as pd
 import numpy as np
+import os
 
 
 #############################################################
@@ -26,7 +27,8 @@ types_names = {90: 'Ia', 67: '91bg', 52:'Iax', 42:'II', 62:'Ibc',
 get_photoids = True
 
 # SALT2 fit is separated by class
-code_plasticc = 90
+code_plasticc = 64
+subtype = 1
 
 subsample = 'DDF'
 
@@ -34,22 +36,21 @@ subsample = 'DDF'
 version = 1
 
 # max number of light curves to fit per run
-chunck_size = 15000          
+chunck_size = 19000       
 
 # path to zenodo test metadata
 test_zenodo_meta = '/media/RESSPECT/data/PLAsTiCC/PLAsTiCC_zenodo/plasticc_test_metadata.csv'
 
+output_dir = '/media/RESSPECT/data/PLAsTiCC/for_metrics/final_data3/' + subsample + '/'
+
 # path to photoids output directory
-photoids_dir = '/media/RESSPECT/data/PLAsTiCC/for_metrics/final_data/' + \
-                subsample +'/SALT2_fit/' + types_names[code_plasticc] + '/photoids/'
+photoids_dir = os.path.join(output_dir, 'SALT2_fit', types_names[code_plasticc], 'photoids')
 
 # path to fitres output directory
-fitres_dir = '/media/RESSPECT/data/PLAsTiCC/for_metrics/final_data/' + \
-                subsample +'/SALT2_fit/' + types_names[code_plasticc] + '/fitres'
+fitres_dir = os.path.join(output_dir, 'SALT2_fit', types_names[code_plasticc], 'fitres')
 
 # path to distances output directory
-fitres_dir = '/media/RESSPECT/data/PLAsTiCC/for_metrics/final_data/' + \
-                subsample +'/SALT2_fit/' + types_names[code_plasticc] + '/distances/'
+dist_dir = os.path.join(output_dir, 'SALT2_fit', types_names[code_plasticc], 'distances')
 
 # path to PLAsTiCC SNANA files
 SNANA_dir = '/media/RESSPECT/data/PLAsTiCC/SNANA/'
@@ -60,9 +61,21 @@ SNANA_dir = '/media/RESSPECT/data/PLAsTiCC/SNANA/'
 restart_master = True
 
 ###############################################################
+# create directories
+
+dirlist = [os.path.join(output_dir, 'SALT2_fit'),
+           os.path.join(output_dir,'SALT2_fit', types_names[code_plasticc]),
+           photoids_dir, fitres_dir, dist_dir]
+
+for name in dirlist:
+    if not os.path.isdir(name):
+        os.makedirs(name)
 
 # translate names
 code_snana = SNANA_types[code_plasticc]
+if not isinstance(code_snana, int):
+    code_snana = SNANA_types[code_plasticc][subtype]
+    
 sntype = types_names[code_plasticc]
 
 if get_photoids:
@@ -77,7 +90,13 @@ if get_photoids:
         ddf_flag = test_metadata['ddf_bool'].values == 0
     
     # get only one class
-    type_flag = test_metadata['true_target'].values == code_plasticc
+    if code_plasticc in [62, 42, 6]:
+        type_flag1 = test_metadata['true_target'].values == code_plasticc
+        type_flag2 = test_metadata['true_submodel'].values == subtype
+        type_flag = np.logical_and(type_flag1, type_flag2)
+    else:
+        type_flag = test_metadata['true_target'].values == code_plasticc
+        
     mask = np.logical_and(ddf_flag, type_flag)
 
     # get all objects of this class in the chosen strategy
@@ -100,14 +119,24 @@ if get_photoids:
     i = 0
     data_chuncks = {}
 
-    for start in range(0, data_out.shape[0], chunk_size):
+    for start in range(0, data_out.shape[0], chunck_size):
         i = i + 1
         
-        upper_lim = min(start + chunk_size, data_out.shape[0])
+        upper_lim = min(start + chunck_size, data_out.shape[0])
         data_chuncks[i] = data_out.iloc[start:upper_lim]
     
-        subset.to_csv(photoids_dir + '/' + sntype + '_' + subsample + \
-                     '_photoids_plasticc_' + str(i) + '.dat', index=False)
+        if code_plasticc in [62, 42, 6]:
+            data_chuncks[i].to_csv(os.path.join(photoids_dir, 
+                                                sntype + '_' + subsample + \
+                                                '_photoids_plasticc_' + \
+                                               str(SNANA_types[code_plasticc][subtype]) + \
+                                               '_' + str(i) + '.dat'), 
+                                                index=False)
+        else:
+            data_chuncks[i].to_csv(os.path.join(photoids_dir, 
+                                                sntype + '_' + subsample + \
+                                                '_photoids_plasticc_' + str(i) + '.dat'), 
+                                                index=False)
 
 
 # perform SALT2 fit     
@@ -115,28 +144,45 @@ start_time = time.time()
     
 print('*********  Fitting light curves   ******************')
 
-fname = photoids_dir + '/' + sntype + '_' + subsample + \
-       '_photoids_plasticc_' + str(version) + '.dat'
+if code_plasticc in [62, 42, 6]:
+    fname = os.path.join(photoids_dir, sntype + '_' + subsample + '_photoids_plasticc_' + \
+                         str(SNANA_types[code_plasticc][subtype]) + '_' + str(version) + '.dat')
+    salt2mu_prefix = 'test_salt2mu_res_' +  str(SNANA_types[code_plasticc][subtype]) + '_'+ str(version)
+    fitres_prefix='test_fitres_' +  str(SNANA_types[code_plasticc][subtype]) + '_' + str(version)
+    salt3_outfile='salt3pipeinput_' +  str(SNANA_types[code_plasticc][subtype]) + '_' + str(version) + '.txt'
+    master_fitres_name=os.path.join(fitres_dir, 'master_fitres_' +  str(SNANA_types[code_plasticc][subtype]) + '_'+ str(version) + '.fitres')
+else:    
+    fname = os.path.join(photoids_dir, sntype + '_' + subsample + '_photoids_plasticc_' + str(version) + '.dat')
+    salt2mu_prefix='test_salt2mu_res_' + str(version)
+    fitres_prefix='test_fitres_' + str(version)
+    salt3_outfile='salt3pipeinput_' + str(version) + '.txt'
+    master_fitres_name=os.path.join(fitres_dir, 'master_fitres_' + str(version) + '.fitres')
 
 meta = pd.read_csv(fname, index_col=False)
 codes = np.unique(meta['code'].values)
+
+
+
 
 res = get_distances(fname,
                      data_prefix='LSST_' + subsample,
                      data_folder=SNANA_dir,            
                      select_modelnum=None,
-                     salt2mu_prefix='test_salt2mu_res_' + str(version),
-                     fitres_prefix='test_fitres_' + str(version),
+                     salt2mu_prefix=salt2mu_prefix,
+                     fitres_prefix=fitres_prefix,
                      maxsnnum=50000,
                      select_orig_sample=['test'],
-                     salt3_outfile='salt3pipeinput_' + str(version) + '.txt',
+                     salt3_outfile=salt3_outfile,
                      data_prefix_has_sntype=False,
-                     master_fitres_name=fitres_dir + '/master_fitres_' + str(version) + '.fitres', 
+                     master_fitres_name=master_fitres_name, 
                      append_master_fitres=True,
                      restart_master_fitres=restart_master)
     
-res['distances'].to_csv(dist_dir + 'mu_' + sntype + '_' + subsample + \
-                        '_plasticc_' + str(version) + '.dat', index=False)
-    
+if code_plasticc in [62, 42, 6]:
+    res['distances'].to_csv(os.path.join(dist_dir,'mu_' + sntype + '_' + subsample + \
+                            '_plasticc_' +  str(SNANA_types[code_plasticc][subtype]) + '_' + str(version) + '.dat'), index=False)
+else:
+    res['distances'].to_csv(os.path.join(dist_dir,'mu_' + sntype + '_' + subsample + \
+                            '_plasticc_' + str(version) + '.dat'), index=False)
         
 print("--- %s seconds ---" % (time.time() - start_time))
